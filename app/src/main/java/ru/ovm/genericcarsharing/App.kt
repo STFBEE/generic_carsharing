c99f9ff6c38a378e9115436e8b44fe75415a31fa
Onishchenko Vladislav <pinkey.950@gmail.com>
2020-11-28 22:21:42 +0300

    Cache for json and photos and day-night theme support in sheet

--- app/src/main/java/ru/ovm/genericcarsharing/App.kt
+++ app/src/main/java/ru/ovm/genericcarsharing/App.kt
@@ -1,8 +1,10 @@
 package ru.ovm.genericcarsharing
 
 import android.app.Application
+import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -14,6 +16,9 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.ovm.genericcarsharing.net.Api
+import ru.ovm.genericcarsharing.utils.Utils
+import java.io.File
+
 
 @Suppress("unused")
 class App : Application() {
@@ -21,11 +26,36 @@ class App : Application() {
     private val appModule = module {
 
         single {
-            OkHttpClient.Builder()
+            val context: Context = get()
+            val httpCacheDirectory = File(context.cacheDir, "responses")
+            val cacheSize: Long = 10 * 1024 * 1024 // 10 MiB
+
+            Cache(httpCacheDirectory, cacheSize)
+        }
+
+        single {
+            val httpClient = OkHttpClient.Builder()
                 .addInterceptor(HttpLoggingInterceptor().apply {
                     level = HttpLoggingInterceptor.Level.BODY
                 })
+                .addInterceptor {
+                    val originalResponse = it.proceed(it.request())
+                    if (Utils.isNetworkConnected(get())) {
+                        val maxAge = 60 // read from cache for 1 minute
+                        originalResponse.newBuilder()
+                            .header("Cache-Control", "public, max-age=$maxAge")
+                            .build()
+                    } else {
+                        val maxStale = 60 * 60 * 24 * 28 // tolerate 4-weeks stale
+                        originalResponse.newBuilder()
+                            .header("Cache-Control", "public, only-if-cached, max-stale=$maxStale")
+                            .build()
+                    }
+                }
+                .cache(get())
                 .build()
+
+            httpClient
         }
 
         single<Gson> {
