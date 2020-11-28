40801d6c93f5648dbbf1299147cc8f5f43f2af99
Onishchenko Vladislav <pinkey.950@gmail.com>
2020-11-28 15:24:18 +0300

    geolocation

--- app/src/main/java/ru/ovm/genericcarsharing/MainActivity.kt
+++ app/src/main/java/ru/ovm/genericcarsharing/MainActivity.kt
@@ -4,15 +4,26 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
 
-    val vm: MainViewModel by viewModel()
+    private val vm: MainViewModel by viewModel()
 
-    @SuppressLint("WrongConstant")
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+    private lateinit var map: MapboxMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,9 +32,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         map_view.onCreate(savedInstanceState)
-        map_view.getMapAsync { mapboxMap ->
-            mapboxMap.setStyle("mapbox://styles/belkacar/ckdj89h8c0rk61jlgb850lece")
-        }
+        map_view.getMapAsync(this)
 
         vm.cars.observe(this) {
             it?.let {
@@ -65,5 +74,53 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         map_view.onDestroy()
+    }
+
+    override fun onMapReady(map: MapboxMap) {
+        this.map = map
+        map.setStyle(Style.Builder().fromUri("mapbox://styles/belkacar/ckdj89h8c0rk61jlgb850lece")) {
+            enableLocationComponent(it)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                    .trackingGesturesManagement(true)
+                    .build()
+
+            val locationComponentActivationOptions = LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                    .locationComponentOptions(customLocationComponentOptions)
+                    .build()
+
+            map.locationComponent.apply {
+                activateLocationComponent(locationComponentActivationOptions)
+                isLocationComponentEnabled = true
+                cameraMode = CameraMode.TRACKING
+                renderMode = RenderMode.COMPASS
+            }
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+        Toast.makeText(this, R.string.toast_location_permission_explanation, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            // может упасть, если стиль будет долго грузится, а юзер прокликает разрешение на пермишены быстрее, но да ладно
+            enableLocationComponent(map.style!!)
+        } else {
+            Toast.makeText(this, R.string.toast_location_permission_not_granted, Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 }
