6752b3fb4f199a327064a22cb699243f455c7a14
Onishchenko Vladislav <pinkey.950@gmail.com>
2020-11-28 14:48:02 +0300

    libs + maps

--- app/src/main/java/ru/ovm/genericcarsharing/MainActivity.kt
+++ app/src/main/java/ru/ovm/genericcarsharing/MainActivity.kt
@@ -1,11 +1,59 @@
 package ru.ovm.genericcarsharing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
         setContentView(R.layout.activity_main)
+
+        map_view.onCreate(savedInstanceState)
+        map_view.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle("mapbox://styles/belkacar/ckdj89h8c0rk61jlgb850lece")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        map_view.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map_view.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map_view.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        map_view.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        map_view.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        map_view.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        map_view.onDestroy()
     }
 }
